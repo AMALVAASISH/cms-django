2079497dd77e3ad43a6e0e3d3342291b5cbd95b2
AMALVAASISH <amalvaasish2000@gmail.com>
2023-11-14 22:44:37 +0530

    late night update

--- cms_django_proj/apibackendapp/admin.py
+++ cms_django_proj/apibackendapp/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Patient,Appointment,ReceptionBill,Gender,BloodGroup, Doctor,Department,Specialization, Staff
+from .models import MedicineDetails,MedicineBill,LabTestManagement,LabBill,LabReport,MedicineHistory,TestPrescription
+from .models import TestPrescriptionTests,MedicinePrescription,MedicinePrescriptionMedicines
+# Register your models here.
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(ReceptionBill)
+admin.site.register(Gender)
+admin.site.register(BloodGroup)
+admin.site.register(Doctor)
+admin.site.register(Department)
+admin.site.register(Specialization)
+admin.site.register(Staff)
+admin.site.register(MedicineDetails)
+admin.site.register(MedicineBill)
+admin.site.register(LabTestManagement)
+admin.site.register(LabBill)
+admin.site.register(LabReport)
+admin.site.register(MedicineHistory)
+admin.site.register(TestPrescription)
+admin.site.register(TestPrescriptionTests)
+admin.site.register(MedicinePrescription)
+admin.site.register(MedicinePrescriptionMedicines)
